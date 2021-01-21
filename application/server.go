5b19aa85072de417d0970e5646499cd4cdd0e205
Fernando Carvalho <fernando.carvalho@e-core.com>
2021-01-21 15:50:50 -0300

    end of http server lesson

---
+++ application/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.processScore(w, r, player)
+	}
+
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (p *PlayerServer) processScore(w http.ResponseWriter, r *http.Request, player string) {
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) GetPlayerScore(name string) int {
+	if name == "Pepper" {
+		return 20
+	}
+
+	if name == "Floyd" {
+		return 20
+	}
+	return 0
+}
