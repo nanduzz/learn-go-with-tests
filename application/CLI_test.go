0e0552b8ad34e697eb1b103e21c84cf995ba976c
Fernando Carvalho <fernando.carvalho@e-core.com>
2021-01-22 15:32:48 -0300

    end of CLI and packge structure lesson

---
+++ application/CLI_test.go
@@ -0,0 +1,32 @@
+package poker_test
+
+import (
+	"strings"
+	"testing"
+
+	poker "github.com/nanduzz/learn-go-with-tests/application"
+)
+
+func TestCli(t *testing.T) {
+
+	t.Run("record Chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record Cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+}
--- application/server_test.go
+++ application/server_test.go
@@ -1,4 +1,4 @@
-package main
+package poker
 
 import (
 	"encoding/json"
@@ -9,24 +9,6 @@ import (
 	"reflect"
 	"testing"
 )
-
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   League
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	return s.scores[name]
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
 
 func TestGetPlayers(t *testing.T) {
 	store := StubPlayerStore{
