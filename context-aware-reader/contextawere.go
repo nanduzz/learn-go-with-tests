fb2fd3d8e0d2b9c00547e3c1e5220fd766bb4adf
Fernando Carvalho <fernando.carvalho@e-core.com>
2021-01-25 15:43:52 -0300

    end of lesson context-aware-reader

---
+++ context-aware-reader/contextawere.go
@@ -0,0 +1,26 @@
+package contextawarereader
+
+import (
+	"context"
+	"io"
+)
+
+type readerCtx struct {
+	ctx      context.Context
+	delegate io.Reader
+}
+
+func NewCancellableReader(ctx context.Context, rdr io.Reader) io.Reader {
+	return &readerCtx{
+		ctx:      ctx,
+		delegate: rdr,
+	}
+}
+
+func (r *readerCtx) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+
+	return r.delegate.Read(p)
+}
